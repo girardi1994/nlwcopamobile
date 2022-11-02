cdfa2c786821cf24513b8fc423c651b5f171149a
girardi1994 <30416996+girardi1994@users.noreply.github.com>
2022-11-02 10:46:49 -0300

    feat: thema, screens, loading

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { NativeBaseProvider, Text, Center, StatusBar } from "native-base";
+import { THEME } from "./src/styles/theme";
+import { Loading } from "./src/components/loading";
+import { SignIn } from "./src/screens/SignIn";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
